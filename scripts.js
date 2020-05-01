4c831f8aa7e886a4db3e9324ca92cbcacdc11771
annguyen28 <ducnguyenan@gmail.com>
2020-05-02 03:18:10 +0900

    test1

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 function validate(){
   var name = document.getElementById("name").value;
-  var subject = document.getElementById("subject").value;
+  
   var phone = document.getElementById("phone").value;
   var email = document.getElementById("email").value;
   var message = document.getElementById("message").value;
@@ -14,22 +14,12 @@ function validate(){
     error_message.innerHTML = text;
     return false;
   }
-  if(subject.length < 10){
-    text = "Please Enter Correct Subject";
-    error_message.innerHTML = text;
-    return false;
-  }
-  if(isNaN(phone) || phone.length != 10){
-    text = "Please Enter valid Phone Number";
-    error_message.innerHTML = text;
-    return false;
-  }
   if(email.indexOf("@") == -1 || email.length < 6){
     text = "Please Enter valid Email";
     error_message.innerHTML = text;
     return false;
   }
-  if(message.length <= 140){
+  if(message.length <= 1){
     text = "Please Enter More Than 140 Characters";
     error_message.innerHTML = text;
     return false;
